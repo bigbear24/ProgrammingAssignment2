0451948d53d69bbdd65d82053c0643c8d3632834
sushant <sushantupadhyay2@gmail.com>
2020-12-19 09:26:43 +0530

    Update cachematrix.R
    # JHU week 3 Assignment Cache Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Concept of Parent/Local Function Enclosure has been put to use over here.
+## Use of '<<-' super Assignment Operator and how it differs from '<-'
+## We have 2 main function , one has the Cached  data (Inverse of Square matrix)
+## If the Inverse(only for invertible) is already present , it will be 
+## produced , else the second Function will compute and update the value 
 
-## Write a short comment describing this function
+## makeCacheMatrix comes with a list of functions to update the value of 
+## Matrix , its inverse value 
+## We have local function enclosures inside Parent function 
+## Super assignment operator makes Changes to both parent environment and 
+## Local environment.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+  m <<- NULL                #makes initial inverse as Null for the first time
+  set <- function(y=matrix()){
+    x <<- y                   #In case you have a new matrix to be computed
+    m <<- NULL                # Set the value of x to the current matrix 
+  }
+  get <- function()x
+  setinv <- function(inv)m<<-inv  # Assign the value of inv to the m 
+  getinv <- function() m
+  
+  list('getinv'=getinv,'setinv'=setinv,'get'=get,'set'=set)
 }
 
 
-## Write a short comment describing this function
+## It will retrieve the last Inverse if already computed for the invertible mat.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+  m<-x$getinv()
+  if(!is.null(m)){
+    print('Getting Cached Data')
+    return(m)
+    
+  }
+  data<-solve(x$get())
+  x$setinv(data)
 }
+
+
